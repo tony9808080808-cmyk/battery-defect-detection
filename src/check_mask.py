b8e684a90aab07864957ff88d9b8a64b58b4e5bc
안세기 <tony9808080808@gmail.com>
2026-04-23 12:19:29 +0900

    Update check_mask.py

--- src/check_mask.py
+++ src/check_mask.py
@@ -5,17 +5,14 @@ from pathlib import Path
 image_dir = Path("data/raw/images")
 mask_dir = Path("data/processed/masks")
 
-# 아무 이미지 하나 선택
 image_path = list(image_dir.glob("*.png"))[0]
 mask_path = mask_dir / image_path.name
 
-# 읽기
 image = cv2.imread(str(image_path))
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
 
-# 시각화
 plt.figure(figsize=(10,5))
 
 plt.subplot(1,2,1)
